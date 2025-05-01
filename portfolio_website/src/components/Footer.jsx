cdffec84fa985205c100fb50650f87f1c135ced5
EddySupra <70074613+EddySupra@users.noreply.github.com>
2025-04-30 19:14:41 -0700

    Delete portfolio_website directory

--- portfolio_website/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="bg-white/80 text-center py-4">
-      <p className="text-sm">© {new Date().getFullYear()} Your Name. All rights reserved.</p>
-    </footer>
-  );
-}
